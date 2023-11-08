d36ef9ab174840ce14f185e1362fe1f091dd2d68
ochotzas <olgerdev@icloud.com>
2023-11-08 04:04:30 +0200

    Remove install_requires

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     author_email='olgerdev@icloud.com',
     url='https://github.com/ochotzas/',
     packages=find_packages(),
-    install_requires=find_packages(),
     python_requires='>=3.9',
     license='MIT',
     classifiers=[
